edece636a647946bee64331eece89e9d4744e240
OleksandrBilenk0 <oleksandrbilenko224@gmail.com>
2023-01-30 10:01:09 +0200

    just key click

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+// keys.forEach(key => key.addEventListener('mousedown', function () {
+//     const audio = new Audio();
+//     audio.src = `audio/${key.keyCode}`;
+//     audio.play();
+//     key.classList.add('playing');
+// }));
+
